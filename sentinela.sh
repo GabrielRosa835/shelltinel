d0a033493d0fe9a137bb432479619737bfc2c887
GabrielRosa835 <gabriel.rosa.080305@gmail.com>
2026-04-26 18:13:08 -0300

    feat: ajusting scripts to accept local utilities (not in PATH)

--- sentinela.sh
+++ sentinela.sh
@@ -12,7 +12,7 @@
 
 # Verificando se há privilégios necessários
 if [ "$(whoami)" != "root" ]; then
-   log -e "obrigatório executar com root"
+   ./log -e "obrigatório executar com root"
    exit 1
 fi
 
@@ -46,21 +46,21 @@ fi
 while [ $interrupt_solicited -eq 0 ]; do
 
    # Consulta 'ps' para o processo de maior consumo da CPU
-   proc_details=$(ps -e -o pid,pcpu,ni,cmd --sort -pcpu | slice 2)
+   proc_details=$(ps -e -o pid,pcpu,ni,cmd --sort -pcpu | ./slice 2)
 
    # Recupera os dados desejados
-   pid=$(           echo "$proc_details" | cut -c 1-7   | trim)
-   uso_cpu=$(       echo "$proc_details" | cut -c 8-13  | trim)
-   proc_niceness=$( echo "$proc_details" | cut -c 14-17 | trim)
-   comando=$(       echo "$proc_details" | cut -c 18-   | trim)
+   pid=$(           echo "$proc_details" | cut -c 1-7   | ./trim)
+   uso_cpu=$(       echo "$proc_details" | cut -c 8-13  | ./trim)
+   proc_niceness=$( echo "$proc_details" | cut -c 14-17 | ./trim)
+   comando=$(       echo "$proc_details" | cut -c 18-   | ./trim)
 
    # Verifica se o processo consome mais de 80% da cpu
    # e se já não possui 'gentileza' 19.
    if [ $proc_niceness -ne 19 ]; then
       if awk -v n1="$uso_cpu" -v n2="80" 'BEGIN{exit !(n1 > n2)}' ; then
          # Altera gentileza e salva o log da ação
-         renice 19 -p $pid | log -i | tee -a $logs
-         log -w "Minimizando prioridade do processo pid:$pid, que estava consumindo ${uso_cpu}%% da CPU com comando '$comando'" | tee -a $logs
+         renice 19 -p $pid | ./log -i | tee -a $logs
+         ./log -w "Minimizando prioridade do processo pid:$pid, que estava consumindo ${uso_cpu}%% da CPU com comando '$comando'" | tee -a $logs
       fi
    fi
 
@@ -68,7 +68,7 @@ while [ $interrupt_solicited -eq 0 ]; do
    heavy_tmp_files=$(find /tmp -type f -size +5M | tr "\n" " ")
    if [ -n "$heavy_tmp_files" ]; then
       # Salva o log se existem e os move para quarentena
-      log -w "Arquivos suspeitos encontrados e movidos para a quarentena: $heavy_tmp_files" | tee -a $logs
+      ./log -w "Arquivos suspeitos encontrados e movidos para a quarentena: $heavy_tmp_files" | tee -a $logs
       mv $heavy_tmp_files $quarentena
 
       # Remove permissões de execução através de seus novos caminhos
@@ -78,13 +78,13 @@ while [ $interrupt_solicited -eq 0 ]; do
    fi
 
    # Consulta uso atual de memória e número de processos em execução
-   free_mem=$(free -m --si | slice 2 | cut -c 27-36 | trim)
+   free_mem=$(free -m --si | ./slice 2 | cut -c 27-36 | ./trim)
    running_procs=$(ps -e | tail -n +2 | wc -l)
 
    # Salva os logs de status
-   log -i "Uso de Memória Atual: ${free_mem}MB" | tee -a $logs
-   log -i "Total de Processos em execução: ${running_procs}" | tee -a $logs
-   log -i "Status do Sentinela: Ativo" | tee -a $logs
+   ./log -i "Uso de Memória Atual: ${free_mem}MB" | tee -a $logs
+   ./log -i "Total de Processos em execução: ${running_procs}" | tee -a $logs
+   ./log -i "Status do Sentinela: Ativo" | tee -a $logs
 
    # Aguarda 30 segundos antes de retornar ao início do loop
    sleep 30
@@ -96,4 +96,4 @@ done
 rm -r $temp
 
 # Exibindo e salvando log de encerramento
-log -i "Sentinela encerrado com segurança" | tee -a $logs
+./log -i "Sentinela encerrado com segurança" | tee -a $logs
